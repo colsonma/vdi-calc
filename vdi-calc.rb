7103cbc24a9c1daddb1d33635792a1d21465a186
Jon Owings <jowings@2vcps.com>
2013-03-09 08:31:02 -0500

    Base Build and beginning Math
    Alot more is needed to display the calculator on the calc_home.erb

--- vdi-calc.rb
+++ vdi-calc.rb
@@ -1,3 +1,53 @@
-#Something I do a lot
-#Places
-puts "30 Iops per desktop and 1000 desktops is how many 15k drives?", (30 * 1000) / 180
+#Something I do a lot- Jon Owings
+#VMware View and VDI Calculator 
+#require 'rubygems'
+require 'sinatra'
+
+vdiType = "VMware View"
+nDesktops = 1000.00
+ioSteady = 10
+conCurr = 1
+maxLC = 4
+avgRam = 2
+memRes = 0.5
+storProto = "NFS"
+userData = 2
+addStor = 4000
+baseImg	= 8
+ioPerBlock = 8200.00
+addDrives = 15
+diskPer = 15
+diskOvr = 0.91
+
+
+maxConCurr = nDesktops * conCurr
+custIO = nDesktops * ioSteady
+numBlk = custIO / ioPerBlock 
+adjNumBlk = numBlk.ceil
+numFlash = adjNumBlk * 2
+numSAS = diskPer * adjNumBlk + addDrives
+capReq = ((avgRam * memRes) + maxLC) * nDesktops /1000
+
+puts "Type of VDI is #{vdiType}"
+puts "Maximum Concurrent Desktops #{maxConCurr}"
+puts "Number of Customer IOps #{custIO}"
+puts "#{numBlk}"
+puts "Number of Additional Drives needed for applications: #{addDrives}"
+puts "Number of VDI Blocks #{adjNumBlk}"
+puts "Number of Flash Drives needed #{numFlash}"
+puts "Number of SAS drives #{numSAS}"
+puts "Capacity Total Required for Linked Clones #{capReq}TB"
+
+
+
+
+get '/' do
+	greeting = "Hello, World!"
+	
+end
+
+get '/calc_home' do
+	erb :calc_home, :locals => {:capReq => params[:capReq]}
+end
+
+
