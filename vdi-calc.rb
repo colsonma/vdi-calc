448102be47859333b5dcf1eaeb2bccc4f8777c30
Jon Owings <jowings@2vcps.com>
2013-03-13 07:57:37 -0400

    Drive Selection and Sizing

--- vdi-calc.rb
+++ vdi-calc.rb
@@ -1,28 +1,10 @@
 #Something I do a lot- Jon Owings
-#VMware View and VDI Calculator 
+#VMware View and Citrix VDI Calculator 
 require 'sinatra'
 require 'rubygems'
-#Samples::
-#vdiType = "VMware View"
-#nDesktops = params[:nDesktops]
-#ioSteady = 10
-#conCurr = params[:conCurr]
-#maxLC = 4
-#avgRam = 2
-#memRes = 0.5
-#storProto = "NFS"
-#userData = 2
-#addStor = 4000
-#baseImg	= 8
+
 ioPerBlock = 8200.00
-#addDrives = 15
-diskPer = 15
-#diskOvr = 0.91
-
-#capReq = ((avgRam * memRes) + maxLC) * nDesktops /1000
-
-
-
+diskOvr = 0.91
 
 
 
@@ -41,15 +23,28 @@ get '/calc_home' do
     @conCurr=params[:conCurr]
     @userData=params[:userData]
     @addStor=params[:addStor]
+    @storProto=params[:storProto]
+    @diskPer=(@vditype == "Citrix PVS" ? "16" : "15")
     @maxConCurr = @nDesktops.to_f * (@conCurr.to_f/100)
     @numBlk= @custIO / ioPerBlock
     @adjNumBlk= @numBlk.ceil
-    @numFlash= @adjNumBlk * 2
-    @addStorage = @addStor.to_f / 300
-    @addDrives= @addStorage.ceil
-    @numSAS= diskPer * @adjNumBlk + @addDrives
-    @capReq= (@avgRam.to_f * @memRes.to_f + @maxLC.to_f) * @nDesktops.to_f/1000
-	erb :calc_home 
+    @hsFlash= (@adjNumBlk * 2)/30
+    @hsFlashUp= (@hsFlash > 1 ? 2 : 1) 
+    @numFlash= (@adjNumBlk * 2) + @hsFlashUp
+    @numSAS= @diskPer.to_f * @adjNumBlk
+    @capReq= ((@avgRam.to_f * (@memRes.to_f/100)) + @maxLC.to_f) * @nDesktops.to_f
+    @hsSAS= @numSAS/30
+    @hsSASUp= @hsSAS.ceil
+    @driveType= (@maxLC.to_f < 3 ? "300" : "600")
+    @dMover= @custIO/12300 + 1
+    @adjMover= @dMover.ceil
+    @dispMover= (@storProto == "NFS" ? @adjMover : "None")
+    @userDataType=(@userData.to_i <= 10000 ? "1TB" : "2TB")
+    @userDataNum=@adjNumBlk * 16
+    @userDataHS = (@userDataNum.to_f/30).ceil
+    @totaluserDataNum= @userDataNum + @userDataHS
+    @vnxType= (@nDesktops < 1501 ? "VNX5300" : (@nDesktops < 3001 ? "VNX5500" : (@nDesktops < 4501 ? "VNX5700" : "VNX7500")))
+    erb :calc_home 
 end
 
 
